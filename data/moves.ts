c571e683e992ae07d50c954c9669fc94f5bf30d2
BeepNBoop <77338022+BeepNBoop@users.noreply.github.com>
2021-02-08 13:30:17 -0500

    Update Venomous on Sludge Bomb

--- data/moves.ts
+++ data/moves.ts
@@ -15775,7 +15775,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {bullet: 1, protect: 1, mirror: 1},
 		onModifyMove(move, pokemon) {
-			if (pokemon.hasAbility('naturalcure')) return;
+			if (pokemon.hasAbility('')) return;
 			move.secondaries = [];
 			if (pokemon.hasAbility('venomous')) {
 				move.secondaries.push({
