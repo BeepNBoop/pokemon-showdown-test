38b046974db3969b16c8e8ea28f176aa3f17a419
BeepNBoop <77338022+BeepNBoop@users.noreply.github.com>
2021-02-19 13:34:54 -0500

    Update Regurgitate

--- data/abilities.ts
+++ data/abilities.ts
@@ -3327,6 +3327,8 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	regurgitation: {
 		onPrepareHit(source, target, move) {
 			if (move.category === 'Status' || move.selfdestruct || move.multihit || move.flags['regurgitate']) return;
+			const originalMove = this.dex.getActiveMove(move.id);
+			this.useMove(originalMove, source);
 			const regurgitate = this.dex.getMove('regurgitate');
 			this.useMove(regurgitate, source);
 			return null;
