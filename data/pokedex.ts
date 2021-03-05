2b8d7c72056f513dade62919f1d8f01b35a61d2c
BeepNBoop <77338022+BeepNBoop@users.noreply.github.com>
2021-03-05 09:47:00 -0500

    Noctem Spiritomb +

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -10757,7 +10757,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		name: "Spiritomb",
 		types: ["Ghost", "Dark"],
 		baseStats: {hp: 50, atk: 92, def: 108, spa: 92, spd: 108, spe: 35},
-		abilities: {0: "Pressure", H: "Infiltrator"},
+		abilities: {0: "Pressure", H: "Noctem"},
 		heightm: 1,
 		weightkg: 108,
 		color: "Purple",
