e2383141669b4eb67b1cf6fbc550ddfe68de5b33
Beep <beepnboopn@gmail.com>
2021-06-15 19:55:54 -0400

    Fix Sliggoo learnset

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -85475,7 +85475,7 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			{generation: 6, level: 30},
 		],
 	},
-	sliggodelta: {
+	sliggoodelta: {
 		learnset: {
 			custommove: ["8L1"], custommovebug: ["8L1"], custommovedark: ["8L1"], custommovedragon: ["8L1"], custommoveelectric: ["8L1"], custommovefairy: ["8L1"], custommovefighting: ["8L1"], custommovefire: ["8L1"], custommoveflying: ["8L1"], custommoveghost: ["8L1"], custommovegrass: ["8L1"], custommoveground: ["8L1"], custommoveice: ["8L1"], custommovepoison: ["8L1"], custommovepsychic: ["8L1"], custommoverock: ["8L1"], custommovesteel: ["8L1"], custommovewater: ["8L1"],
 			shoreup: ["8L1"],
